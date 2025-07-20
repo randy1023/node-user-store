0cc34d4696125d101669529445f3c2876d2b88e2
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-07-20 11:47:28 -0400

    configuracion principal

---
+++ src/config/envs.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import * as env from "env-var";
+
+export const envs = {
+  PORT: env.get("PORT").required().asPortNumber(),
+  PUBLIC_PATH: env.get("PUBLIC_PATH").default("public").asString(),
+};
