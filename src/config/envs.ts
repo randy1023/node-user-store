aed1c555aa4e745aef32c8b613e016978dcef746
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-08-01 12:42:45 -0400

    feat(email.service):Se agrego la verificacion de email

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,4 +7,8 @@ export const envs = {
   MONGO_URL: env.get("MONGO_URL").required().asString(),
   MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
   JWT_SEED: env.get("JWT_SEED").required().asString(),
+  MAILER_SERVICE: env.get("MAILER_SERVICE").default("gmail").asString(),
+  MAILER_EMAIL: env.get("MAILER_EMAIL").required().asEmailString(),
+  MAILER_SECRET_KEY: env.get("MAILER_SECRET_KEY").required().asString(),
+  WEBSERVICE_URL: env.get("WEBSERVICE_URL").required().asUrlString(),
 };
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { bcryptAdapter, JwtAdapter } from "../../config";
+import { bcryptAdapter, envs, JwtAdapter } from "../../config";
 import { UserModel } from "../../data";
 import {
   CustomError,
@@ -6,9 +6,13 @@ import {
   RegisterUserDto,
   UserEntity,
 } from "../../domain";
+import { EmailService } from "../services";
 
 export class AuthService {
-  constructor() {} // Replace 'any' with the actual type of your UserRepository
+  constructor(
+    // DI - Email Service
+    private readonly emailService: EmailService
+  ) {} // Replace 'any' with the actual type of your UserRepository
 
   public async registerUser(registerUserDto: RegisterUserDto) {
     const existUser = await UserModel.findOne({
@@ -21,15 +25,23 @@ export class AuthService {
       // Encriptar la contraseña
       user.password = await bcryptAdapter.hash(registerUserDto.password);
       await user.save();
-      //JWT <---- para mantener la autenticación del usuario
-
       // Email de confirmacion
+      //??? Como usar el serviciode enviar correo de confirmacion
+      await this.sendEmailValidationLink(user.email);
+      //JWT <---- para mantener la autenticación del usuario
+      const payload = {
+        id: user.id,
+      };
+      const token = await JwtAdapter.generateToken(payload);
+
+      if (!token)
+        throw CustomError.internalServerError("Error while creating JWT");
 
       // de esta manera se evita devolver el password es una de tantas maneras
       const { password, ...restData } = UserEntity.fromObject(user);
       return {
         user: restData,
-        token: "ABC",
+        token,
       };
     } catch (error) {
       throw CustomError.internalServerError(`${error}`);
@@ -64,4 +76,29 @@ export class AuthService {
       throw CustomError.internalServerError(`${error}`);
     }
   }
+  private sendEmailValidationLink = async (email: string) => {
+    const token = await JwtAdapter.generateToken({ email });
+    if (!token)
+      throw CustomError.internalServerError("Error while creating JWT");
+
+    const link = `${envs.WEBSERVICE_URL}/auth/validate-email/${token}`;
+
+    const html = `
+    <h1> Validate your email</h1>
+    <p>Click on the following link to validate your email</p>
+    <a href="${link}">Press to Validate your email</a>
+    `;
+
+    const options = {
+      to: email,
+      subject: "Validate to email",
+      htmlBody: html,
+    };
+
+    const isSet = await this.emailService.sendEmail(options);
+
+    if (!isSet) throw CustomError.internalServerError("Error sending email");
+
+    return true;
+  };
 }
