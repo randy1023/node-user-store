e6d8e6f55e64d1164ada632c6f967508e27d9fa1
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-11-18 14:36:33 -0400

    add: model product

---
+++ src/data/mongo/models/product.model.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true,
+  },
+  available: {
+    type: Boolean,
+    default: false,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+})
+
+export const ProductModel = mongoose.model('Product', productSchema)
